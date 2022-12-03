54093fac804525407d8f7b3a8e69002ef66a4a1f
Cacaonib2020 <91684527+Cacaonib2020@users.noreply.github.com>
2022-12-04 01:32:26 +0900

    時報音実装

--- Main.cpp
+++ Main.cpp
@@ -2,57 +2,29 @@
 
 void Main()
 {
-	// 背景の色を設定 | Set background color
-	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	auto nowtime = DateTime::Now();//現在時刻を格納
+	const Font degi{50};//時計オブジェクトを設定
+	auto lastsay = nowtime;//最終読み上げ時刻を格納
+	TextToSpeech::SetDefaultLanguage(LanguageCode::Japanese);//Sayする言語を設定
 
-	// 通常のフォントを作成 | Create a new font
-	const Font font{ 60 };
+	const Audio sinelow { U"C:/Users/yuzu6/WorkSpace/PlayGround/C++/Zihou/Tone/500hz.wav"};
+	const Audio sinemid { U"C:/Users/yuzu6/WorkSpace/PlayGround/C++/Zihou/Tone/1000hz.wav" };
+	const Audio sinehigh{ U"C:/Users/yuzu6/WorkSpace/PlayGround/C++/Zihou/Tone/2000hz.wav"};
 
-	// 絵文字用フォントを作成 | Create a new emoji font
-	const Font emojiFont{ 60, Typeface::ColorEmoji };
-
-	// `font` が絵文字用フォントも使えるようにする | Set emojiFont as a fallback
-	font.addFallback(emojiFont);
-
-	// 画像ファイルからテクスチャを作成 | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
-
-	// 絵文字からテクスチャを作成 | Create a texture from an emoji
-	const Texture emoji{ U"🐈"_emoji };
-
-	// 絵文字を描画する座標 | Coordinates of the emoji
-	Vec2 emojiPos{ 300, 150 };
-
-	// テキストを画面にデバッグ出力 | Print a text
-	Print << U"Push [A] key";
-
-	while (System::Update())
+	Say << Format(nowtime).substr(11,12);//現在時刻を読み上げ
+	while (TextToSpeech::IsSpeaking());//読み上げ終了まで待機
+	while (System::Update())//本文
 	{
-		// テクスチャを描く | Draw a texture
-		texture.draw(200, 200);
+		nowtime = DateTime::Now();//現在時刻を更新
+		degi(nowtime).draw(20, 20);//時計オブジェクトを設置＆更新
 
-		// テキストを画面の中心に描く | Put a text in the middle of the screen
-		font(U"Hello, Siv3D!🚀").drawAt(Scene::Center(), Palette::Black);
+		//Console << Format(nowtime.second % 30 - 27);//デバッグコンソールに最終読み上げ時刻を出力
+		if (nowtime.second != lastsay.second) {
+			lastsay = nowtime;//最終読み上げ時刻を更新
 
-		// サイズをアニメーションさせて絵文字を描く | Draw a texture with animated size
-		emoji.resized(100 + Periodic::Sine0_1(1s) * 20).drawAt(emojiPos);
-
-		// マウスカーソルに追随する半透明な円を描く | Draw a red transparent circle that follows the mouse cursor
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1, 0, 0, 0.5 });
-
-		// もし [A] キーが押されたら | When [A] key is down
-		if (KeyA.down())
-		{
-			// 選択肢からランダムに選ばれたメッセージをデバッグ表示 | Print a randomly selected text
-			Print << Sample({ U"Hello!", U"こんにちは", U"你好", U"안녕하세요?" });
-		}
-
-		// もし [Button] が押されたら | When [Button] is pushed
-		if (SimpleGUI::Button(U"Button", Vec2{ 640, 40 }))
-		{
-			// 画面内のランダムな場所に座標を移動
-			// Move the coordinates to a random position in the screen
-			emojiPos = RandomVec2(Scene::Rect());
+			if (nowtime.second % 10 == 0) {sinemid.playOneShot();}
+			else if ((nowtime.second % 30 - 27) >= 0) {sinelow.playOneShot();}
+			sinehigh.playOneShot();
 		}
 	}
 }
